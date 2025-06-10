f8ea8516789f72b0ce39f751d2d34a04ffc89017
benar69 <aryarimansyah@gmail.com>
2025-06-10 21:38:07 +0700

    Modification for new aht10 implementation and esp01 board

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,9 @@
 #include <Arduino.h>
-// #include <IRremoteESP8266.h>
-// #include <IRsend.h>
 #include <ir_Sharp.h>
+#include <Wire.h>
+#include <Adafruit_AHTX0.h>
 
-const uint16_t kIrLed = 14;  // ESP8266 GPIO pin to use. Recommended: 4 (D2).
-IRSharpAc ac(kIrLed);  // Set the GPIO to be used for sending messages.
-
-#include "DHT.h"
-
-#define DHTPIN 12
-#define DHTTYPE DHT22 
-DHT dht(DHTPIN, DHTTYPE);
-
+#define IR_LED_PIN  3
 
 #define BOARD_BUTTON_PIN 4
 #define BOARD_BUTTON_ACTIVE_LOW true
@@ -20,7 +12,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #define BOARD_LED_BRIGHTNESS 255
 
 #define BLYNK_TEMPLATE_ID "TMPL6Y5x4UMja"
-#define BLYNK_TEMPLATE_NAME "Room Controller"
+#define BLYNK_TEMPLATE_NAME "SharpAC Switch"
 
 #define BLYNK_FIRMWARE_VERSION                                                                                                                                                                                                                                                                                                                                                                                                "0.1.0"
 
@@ -33,137 +25,54 @@ DHT dht(DHTPIN, DHTTYPE);
 
 BlynkTimer timer;
 
-#include <TimeLib.h>
-time_t updated_local_time;
-
-void printState() {
-  // Display the settings.
-  Serial.println("Sharp A/C remote is in the following state:");
-  Serial.printf("  %s\n", ac.toString().c_str());
-  // Display the encoded IR sequence.
-  unsigned char* ir_code = ac.getRaw();
-  Serial.print("IR Code: 0x");
-  for (uint8_t i = 0; i < kSharpAcStateLength; i++)
-    Serial.printf("%02X", ir_code[i]);
-  Serial.println();
-}
+IRSharpAc ac(IR_LED_PIN);  // Set the GPIO to be used for sending messages.
+Adafruit_AHTX0 aht;
 
 void initAC() {
   ac.begin();
-
-  // kGreeAuto, kGreeDry, kGreeCool, kGreeFan, kGreeHeat
-  ac.setMode(kSharpAcCool);
-  ac.setFan(kSharpAcFanAuto);
-  ac.setTemp(26);  // 16-30C
-  ac.setSwingV(kSharpAcSwingVCoanda, false);
-  ac.setIon(true);
-  ac.setEconoToggle(true);
-
-  Serial.println("Default state of the remote.");
-  printState();
-  Serial.println("Setting desired state for A/C.");
 }
 
 void updateSensorData() {
-  float t = dht.readTemperature();
-  float h = dht.readHumidity();
+  sensors_event_t humidity, temp;
+  aht.getEvent(&humidity, &temp);
+
+  float t = temp.temperature;
+  float h = humidity.relative_humidity;
+
   if (isnan(h) || isnan(t)) {
-    Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
   else {
-    Serial.print(F("Humidity: "));
-    Serial.print(h);
-    Serial.print(F("%  Temperature: "));
-    Serial.print(t);
-    Serial.println();
-
     Blynk.virtualWrite(V0, t);
     Blynk.virtualWrite(V1, h);
   }
 }
 
-void dailyACturnOff() {
-  if ( (hour(updated_local_time) == 4) && (minute(updated_local_time) == 30) && ac.getPower()) {
-    Blynk.virtualWrite(V2, 0);
-    ac.off();
-    ac.send(); 
-  }
-}
-
 void timerFunction() {
   Blynk.sendInternal("rtc", "sync"); //request current local time for device
-  Serial.println();
-  Serial.printf("Local time: %02d:%02d:%02d\n", hour(updated_local_time), minute(updated_local_time),second(updated_local_time));
   updateSensorData();
-  dailyACturnOff();
 }
 
 BLYNK_CONNECTED() {
   Blynk.syncVirtual(V2);
   Blynk.sendInternal("rtc", "sync"); //request current local time for device
-  dailyACturnOff();
 }
 
 BLYNK_WRITE(InternalPinRTC) {   //check the value of InternalPinRTC  
   unsigned long t = param.asLong();      //store time in t variable
-  updated_local_time = t;
 }
 
 BLYNK_WRITE(V2)
-{   
-  if(param.asInt())
-  {
-    ac.on();
-    Serial.println("Turning ON AC");
-  }
-  else
-  {
-    ac.off();
-    Serial.println("Turning OFF AC");
-  }
-  
-  ac.send(); 
-}
-
-BLYNK_WRITE(V3)
-{ 
-  int setTemperature = param.asInt();
-  ac.setTemp(setTemperature);
-  Serial.println("Temperature set to: " + String(setTemperature) + "C");
-  
-  if (ac.getPower())  
-  {
-    ac.send();
-  }
-}
-
-
-BLYNK_WRITE(V4)
-{   
-  if(param.asInt())  
-  {
-    ac.setMode(kSharpAcDry);
-    Serial.println("set to Dry mode");
-  }
-  else  
-  {
-    ac.setMode(kSharpAcCool);
-    Serial.println("set to Cool mode");
-  }
-
-  if (ac.getPower())  
-  {
-    ac.send();
-  }
+{  
+  if(param.asInt() == 1)  ac.on();
+  else  ac.off();
+  ac.send();
 }
 
 void setup() {
-  Serial.begin(115200);
-  delay(200);
-
   initAC();
-  dht.begin();
+  Wire.begin(2,0);
+  aht.begin();
 
   BlynkEdgent.begin();
 
